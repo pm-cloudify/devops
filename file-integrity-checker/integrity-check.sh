aa446ae33ab7249fd602f23bd74d13d775e3d6a5
Pouya <pouyamohammadyirbu@gmail.com>
2025-09-28 07:47:48 +0330

    chore: improve script structure

--- file-integrity-checker/integrity-check.sh
+++ file-integrity-checker/integrity-check.sh
@@ -1,27 +1,65 @@
 #!/usr/bin/bash
 
+# Input commands
 cmd="$1"
 path="$2"
 
-# TODO: check hash integrity
-checkHash() {
+# Base of hash
+BaseHashStore="/usr/local/share/hash/store/"
+mkdir -p "$BaseHashStore"
+
+# ********** Functions *************
+checkPath(){
+    local path="$1"
+    if [ -d "$path" ]; then
+        # echo "This is a dir"
+        return 1
+    elif [ -f "$path" ]; then
+        # echo "This is a file"
+        return 2
+    else
+        # echo "wrong path input"
+        return 3
+    fi
+}
+
+# check hash integrity
+check() {
+    local path="$1"
+}
+
+# update hash of log
+update() {
+    local filePath="$1"
+    local res=$(sha256sum "filePath" | awk '{ print $1 }')
+    return "$res"
+}
+
+
+# update this file hashes
+update() { 
     echo ""
 }
 
-# TODO: update hash of log
-updateHash() {
-    echo ""
+# help message
+_help() {
+    echo -e "Invalid option: $1 \n    • Use 'init', 'update', or 'check'."
 }
 
-# TODO: check and run cmd
-runCommand() { 
-    echo ""
-}
+# ********** End of functions 
 
-if [ -d "$path" ]; then
-    echo "This is a dir"
-elif [ -f "$path" ]; then
-    echo "This is a file"
-else
-    echo "wrong path input"
-fi
+# cmd run
+case "$1" in 
+    init)
+        init "$path"
+        ;;
+    check)
+        check "$path"
+        ;;
+    update)
+        init "$path"
+        ;;
+    *)
+        _help "$cmd"
+        ;;
+esac
